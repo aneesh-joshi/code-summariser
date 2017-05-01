e4dcdfbcaf2cae79512566b19c673d3e448df397
ykl7 <yklal95@gmail.com>
2017-05-01 18:52:52 +0530

    server

--- write_to_file.py
+++ write_to_file.py
@@ -1,6 +1,6 @@
-f = open('libgdx_test_shuffled_output.json', "rb")
+f = open('implementation/libgdx_test_shuffled_output.json', "rb")
 
-out = open('libgdx_output_small.json', "w+")
+out = open('implementation/libgdx_output_small.json', "w+")
 
 out.write(f.read(1024*16))
 
